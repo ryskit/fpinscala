64c7e1f6525ce748024e479cc35d08db3e35577a
ryskit <ryosuke.kida@zozo.com>
2022-12-09 12:18:22 +0900

    done EXERCISE3.27, 3.28

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -17,11 +17,29 @@ object Tree {
   }
 
   // EXERCISE 3.26
-  // Tre[Int]の最大の要素を返すmaximum関数を記述せよ。
+  // Tree[Int]の最大の要素を返すmaximum関数を記述せよ。
   def maximum(tree: Tree[Int]): Int = {
     tree match {
       case Leaf(value)         => value
       case Branch(left, right) => maximum(left) max maximum(right)
+    }
+  }
+
+  // EXERCISE 3.27
+  // 2分木のルートから任意のLeafまでの最長パスを返すdepth関数を記述せよ。
+  def depth[A](tree: Tree[A]): Int = {
+    tree match {
+      case Leaf(_)             => 1
+      case Branch(left, right) => 1 + (depth(left) max depth(right))
+    }
+  }
+
+  // EXERCISE 3.28
+  // 2分木の各要素を特定の関数を使って変更するmap関数を記述せよ。
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+    tree match {
+      case Leaf(value)         => Leaf(f(value))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
     }
   }
 }
--- src/test/scala/datastructures/TreeSpec.scala
+++ src/test/scala/datastructures/TreeSpec.scala
@@ -56,4 +56,44 @@ class TreeSpec extends AnyFunSpec {
       assert(Tree.maximum(tree) == expected)
     }
   }
+
+  describe("EXERCISE3.27") {
+    it("max number is 1") {
+      val tree     = Leaf(1)
+      val expected = 1
+      assert(Tree.depth(tree) == expected)
+    }
+
+    it("max depth is 2") {
+      val tree     = Branch(Leaf(1), Leaf(2))
+      val expected = 2
+      assert(Tree.depth(tree) == expected)
+    }
+
+    it("max depth is 3") {
+      val tree     = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+      val expected = 3
+      assert(Tree.depth(tree) == expected)
+    }
+
+    it("max number is 4") {
+      val tree     = Branch(Branch(Branch(Leaf(2), Leaf(3)), Leaf(11)), Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+      val expected = 4
+      assert(Tree.depth(tree) == expected)
+    }
+  }
+
+  describe("EXERCISE3.28") {
+    it("convert Int to String") {
+      val tree     = Leaf(1)
+      val expected = Leaf("1")
+      assert(Tree.map(tree)(_.toString) == expected)
+    }
+
+    it("multiple 2") {
+      val tree     = Branch(Leaf(1), Leaf(2))
+      val expected = Branch(Leaf(2), Leaf(4))
+      assert(Tree.map(tree)(_ * 2) == expected)
+    }
+  }
 }
