0e05c9556001afe23e48a9a7825849b11437f4e3
ryskit <ryosuke.kida@zozo.com>
2023-01-27 11:18:46 +0900

    done EXERCISE4.5

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -85,4 +85,15 @@ object Option {
       map2(v, acc)(_ :: _)
     }
 
+  def parseInts(a: List[String]): Option[List[Int]] =
+    sequence(a map (i => Try(i.toInt)))
+
+  // EXERCISE4.5
+  // traverse関数を実装せよ。
+  // mapとsequenceを使用すれば簡単だが、リストを1回だけ調べる、より効率のよい実装にすること。
+  // 要するに、traverseの観点からsequenceを実装すればよい
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil)) { case (v, acc) =>
+      map2(f(v), acc)(_ :: _)
+    }
 }
