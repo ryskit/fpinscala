2b663fdc5ac999e933e8da740eb60320b07cf9b2
ryskit <ryosuke.kida@zozo.com>
2023-01-27 11:48:53 +0900

    EXERCISE4.6

---
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -0,0 +1,41 @@
+package fpinscala.errorhandling
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(v) => Right(f(v))
+      case Left(e)  => Left(e)
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(v) => f(v)
+      case Left(e)  => Left(e)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(v) => Right(v)
+      case Left(_)  => b
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      aa <- this
+      bb <- b
+    } yield f(aa, bb)
+}
+case class Left[+E](value: E)  extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty) Left("mean of empty list!")
+    else Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch {
+      case e: Exception => Left(e)
+    }
+}
